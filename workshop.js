8e02bda1975fbfe810d69190f664355688256264
Camille Webb <camillewebb35@gmail.com>
2024-07-30 19:38:51 -0600

    practicing js with a vault code

---
+++ workshop.js
@@ -0,0 +1,12 @@
+// Process: Define the variable to start the message. Then define the numbers of the vault combination using different arithmetic operators. Set an alert that concatenates the phrase and numbers. 
+const greet = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// The first number of the combination is 10, so here is an arithmetic operation to get that result. 
+let num1 = 6 + 4
+// The second number in the combo is 40
+let num2 = 8 * 5
+// The final number in the combo is 39
+let num3 = 40 - 1
+
+// Let's put it all together and create a popup dialog box with the message.
+alert(greet + " " + num1 + " - " + num2 + " - " + num3)
